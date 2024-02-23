1ef002d9c95bad0647e7af2b7ac74672d4ec1831
RDegnen <rdegnen@gmail.com>
2024-02-22 20:18:37 -0700

    images are "zoomable"

--- tradingjournal.client/src/pages/TradesPage/components/NotesAndImagesModal/index.tsx
+++ tradingjournal.client/src/pages/TradesPage/components/NotesAndImagesModal/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useCallback, useState } from 'react'
-import { Grid, Box } from "@mui/material";
+import { Grid, Box, ImageList } from "@mui/material";
 import Trade from "../../../../models/trade";
+import ZoomableImageListItem from './ZoomableImage'
 
 interface NotesAndImagesModalProps {
   data: Trade
@@ -13,18 +14,6 @@ interface PreSignedUrlsResponseObject {
 export default function NotesAndImagesModal(props: NotesAndImagesModalProps) {
   const { notes, imageLocations } = props.data
   const [images, setImages] = useState<string[]>([])
-
-  /**
-   * How loading presigned urls could work
-   * - This modal gets opened
-   * - It takes all the image keys can gets the presigned urls from the backend
-   * - With those urls it fetches the images
-   * - WITHOUT CACHING it renders each image
-   * - WITH CACHING it updates a trade with the images and then renders them here
-   * so that every time a modal is closed and reopened it does not have to refetch the
-   * images. Or maybe there can be an image cache that only holds those and the trade
-   * id...
-   */
 
   const getPreSignedUrls = useCallback(async () => {
     const queries = imageLocations.map(key => ['imageKeys', key])
@@ -57,7 +46,7 @@ export default function NotesAndImagesModal(props: NotesAndImagesModalProps) {
       top: '50%',
       left: '50%',
       transform: 'translate(-50%, -50%)',
-      width: 800,
+      width: 1000,
       bgcolor: 'background.paper',
       border: '2px solid #000',
       boxShadow: 24,
@@ -67,14 +56,14 @@ export default function NotesAndImagesModal(props: NotesAndImagesModalProps) {
         <Grid item xs={6}>
           {notes}
         </Grid>
-        <Grid item container spacing={2} xs={6} rowSpacing={1}>
-          {images.map(url => {
-            return (
-              <Grid item xs={4} sx={{ width: 100, height: 80 }} key={url}>
-                <img src={url} style={{ width: '100%', height: '100%' }} />
-              </Grid>
-            )
-          })}
+        <Grid item xs={6}>
+          <ImageList cols={3}>
+            {images.map(url => {
+              return (
+                <ZoomableImageListItem key={url} src={url} />
+              )
+            })}
+          </ImageList>
         </Grid>
       </Grid>
     </Box>
